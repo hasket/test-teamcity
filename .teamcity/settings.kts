e82e49bfc39b4042d5c5068eaf35d0ab4e2fb9f7
AAvazerov <artem_vazerov@rgs.ru>
2022-05-25 22:31:43 +0300

    add low script

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,45 +1,17 @@
 import jetbrains.buildServer.configs.kotlin.*
-import jetbrains.buildServer.configs.kotlin.branchRemoteRun
-import jetbrains.buildServer.configs.kotlin.buildSteps.python
 import jetbrains.buildServer.configs.kotlin.buildSteps.script
 
 version = "2022.04"
 
 project {
-    buildType(AnsibleVersion2)
+    buildType(HelloWorld)
 }
 
-object AnsibleVersion2 : BuildType({
-    name = "ansiblev2"
-
-    vcs {
-        root(Ansible_1)
-    }
-
+object HelloWorld: BuildType({
+    name = "Hello world"
     steps {
         script {
-            scriptContent = "source ./hacking/env-setup"
-        }
-        python {
-            environment = venv {
-            }
-            command = flake8 {
-                scriptArguments = "--config=setup.cfg"
-            }
-        }
-        python {
-            environment = venv {
-            }
-            command = file {
-                filename = "setup.py"
-                scriptArguments = "install --user --install-scripts=."
-            }
-        }
-    }
-
-    triggers {
-        branchRemoteRun {
-            gitBranchPattern = "refs/heads/remote-run/devel"
+            scriptContent = "echo 'Hello world!'"
         }
     }
 })
