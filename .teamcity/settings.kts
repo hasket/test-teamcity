63194150c86334f6538ee4953bb64f5ad3ee96bd
AAvazerov <artem_vazerov@rgs.ru>
2022-06-01 08:57:13 +0300

    sdfg

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,19 +1,49 @@
-import jetbrains.buildServer.configs.kotlin.v2019_2.*
-import jetbrains.buildServer.configs.kotlin.v2019_2.branchRemoteRun
-import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.python
-import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
+import jetbrains.buildServer.configs.kotlin.v10.buildSteps.XcodeStep
+import jetbrains.buildServer.configs.kotlin.v2019_2.vcs.*
+
 
 version = "2022.04"
 
 project {
     buildType(HelloWorld)
+    buildType(Ansible)
 }
 
 object HelloWorld: BuildType({
     name = "Hello world"
     steps {
         script {
-            scriptContent = "echo 'Hello world!'"
+            command = "echo 'Hello world!'"
+        }
+        script {
+
+        }
+    }
+})
+
+object Ansible: BuildType({
+    name = "ansible"
+
+    vcs = 
+
+    steps {
+        script {
+            scriptContent = "source ./hacking/env-setup"
+        }
+        python {
+            environment = venv {
+            }
+            command = flake8 {
+                scriptArguments = "--config=setup.cfg"
+            }
+        }
+        python {
+            environment = venv {
+            }
+            command = file {
+                filename = "setup.py"
+                scriptArguments = "install --user --install-scripts=."
+            }
         }
     }
 })
